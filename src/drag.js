9cae0093dabd070f5e0a6dfafbc9e31ad3149124
Juho Vepsalainen <bebraw@gmail.com>
2012-07-09 12:35:17 +0300

    Remove debug print

--- src/drag.js
+++ src/drag.js
@@ -221,7 +221,7 @@ function findPos(e) {
 function cursorX(elem, evt) {
     if(isFixed(elem)) {
         var bodyLeft = parseInt(document.defaultView.getComputedStyle(document.body, "").marginLeft, 10) - calc(elem, 'scrollLeft') + elem.style.marginLeft;
-console.log(elem.style);
+
         return evt.clientX - bodyLeft;
     }
     if(evt.pageX) return evt.pageX;
