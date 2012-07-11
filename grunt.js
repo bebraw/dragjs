ef38b51135f4a49004906c12b4401bcac0a90636
Juho Vepsalainen <bebraw@gmail.com>
2012-07-11 12:50:00 +0300

    Set up grunt build

---
+++ grunt.js
@@ -0,0 +1,37 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.6.1',
+      banner: '/*! colorjoe - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://bebraw.github.com/drag.js/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'Juho Vepsalainen; Licensed MIT */'
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>',
+              'src/drag.js'],
+        dest: 'dist/drag.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/drag.min.js'
+      }
+    },
+    watch: {
+      files: 'src/**/*.js',
+      tasks: 'concat min'
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'concat min');
+
+};
