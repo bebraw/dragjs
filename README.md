229df6d0e978ffdee92059944cbbaca9a26772fc
Juho Vepsäläinen <bebraw@gmail.com>
2021-11-16 11:50:01 +0200

    chore: Port build to dnt

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ dragjs makes it easy to write deal with dragging in JS. Handy for draggables, sl
 * [Jean Carrière](https://github.com/JeanCarriere)
 * [Sam Potts](https://github.com/SamPotts) - Use getBoundingClientRect(), IE9+ support
 
+## Development
+
+To build, run `deno run -A scripts/buildNpm.ts 0.1.0`. Adjust version accordingly. Then to publish, `cd npm && npm publish`.
+
 ## License
 
 dragjs is available under MIT. See LICENSE for more details.
-
---
+++ scripts/buildNpm.ts
@@ -0,0 +1,26 @@
+// ex. scripts/build_npm.ts
+import { build } from "https://deno.land/x/dnt@0.7.0/mod.ts";
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    // package.json properties
+    name: "dragjs",
+    version: Deno.args[0],
+    description: "Simple utility to make it easier to implement drag based things (ie. sliders and such)",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/bebraw/dragjs.git",
+    },
+    bugs: {
+      url: "https://github.com/bebraw/dragjs/issues",
+    },
+    keywords: ["drag", "dragging", "draggable"]
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
